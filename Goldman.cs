800c72a38e65a6f9c839841f2ddb13833859e508
arman <arman97rm@gmail.com>
2021-02-25 14:17:13 -0500

    goldman questions

---
+++ Goldman.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class Goldman
+    {
+        public static int TrappingRainwater(int[] height)
+        {
+            List<int> waterLevel = new List<int>();
+            int curr = 0;
+            for (int i = 0; i < height.Length; i++)
+            {
+                if (height[i] > curr) curr = height[i];
+                waterLevel.Add(curr);
+            }
+            curr = 0;
+            for (int j = height.Length - 1; j >= 0; j--)
+            {
+                if (height[j] > curr) curr = height[j];
+                if (waterLevel[j] > curr) waterLevel[j] = curr;
+            }
+            int bucket = 0;
+            for (int k = 0; k < height.Length; k++)
+            {
+                int water = waterLevel[k] - height[k];
+                bucket += water;
+            }
+            return bucket;
+        }
+
+        public static int[][] HighFive(int[][] items)
+        {
+            SortedDictionary<int, List<int>> map = new SortedDictionary<int, List<int>>();
+            for (int i = 0; i < items.Length; i++)
+            {
+                int id = items[i][0];
+                int score = items[i][1];
+                if (!map.ContainsKey(id))
+                {
+                    map.Add(id, new List<int>());
+                }
+                map[id].Add(score);
+            }
+            int[][] result = new int[map.Count][];
+            int j = 0;
+            foreach (KeyValuePair<int, List<int>> kvp in map)
+            {
+                int total = 0;
+                kvp.Value.Sort();
+                kvp.Value.Reverse();
+                for (int m=0; m<5; m++)
+                {
+                    total += kvp.Value[m];
+                }
+                int avg = total / 5;
+                result[j] = new int[] { kvp.Key, avg };
+                j++;
+            }
+            return result;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,81 +4,23 @@ using System.Collections.Generic;
 
 namespace LeetCode
 {
-    /*
-     *      4
-     *    2   6
-     *    2 4 6
-     * 
-     *      1
-     *    2   6
-     * 2 1 6
-     */
-    class Program
-    {
-
+    class Program {
         static void Main(string[] args)
         {
-            Console.WriteLine(mskcc(-999));
-        }
-
-        public static int mskcc(int N)
-        {
-            List<int> list = new List<int>();
-            bool neg = N < 0;
-            while (N != 0)
-            {
-                list.Add(Math.Abs(N%10));
-                N /= 10;
-            }
-            int pw = list.Count,ans=0;
-            bool added = false;
-            for(int i=list.Count-1; i>=0; i--)
-            {
-                if (neg && list[i] > 5 && !added)
-                {
-                    ans += (5 * (int)Math.Pow(10, pw));
-                    pw--;
-                    added = true;
-                }
-                else if(list[i] < 5 && !added)
-                {
-                    ans += (5 * (int)Math.Pow(10, pw));
-                    pw--;
-                    added = true;
-                }
-                ans+= (list[i] * (int)Math.Pow(10, pw));
-                pw--;
-            }
-            if (neg) ans *= -1;
-            return ans;
-            //StringBuilder ans = new StringBuilder();
-            //if (N < 0) ans.Append('-');
-            //string n = N.ToString();
-            //if (N < 0) n = n.Substring(1);
-            //for (int i = 0; i < n.Length; i++)
-            //{
-            //    int temp = (int)n[i] - 48;
-            //    if (N >= 0)
-            //    {
-            //        if (temp < 5)
-            //        {
-            //            ans.Append('5');
-            //            ans.Append(n.Substring(i));
-            //            break;
-            //        }
-            //    }
-            //    else
-            //    {
-            //        if (temp > 5)
-            //        {
-            //            ans.Append('5');
-            //            ans.Append(n.Substring(i));
-            //            break;
-            //        }
-            //    }
-            //    ans.Append(n[i]);
-            //}
-            //return ans.ToString();
+            int[] x = new int[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+            int[][] y = new int[10][];
+            y[0]=new int[] {1,100 } ;
+            y[1] = new int[] { 7, 100 };
+            y[2] = new int[] { 1, 100 };
+            y[3] = new int[] { 7, 100 };
+            y[4] = new int[] { 1, 100 };
+            y[5] = new int[] { 7, 100 };
+            y[6] = new int[] { 1, 100 };
+            y[7] = new int[] { 7, 100 };
+            y[8] = new int[] { 1, 100 };
+            y[9] = new int[] { 7, 100 };
+            // Display the number of command line arguments.
+            Goldman.HighFive(y);
         }
     }
 }
